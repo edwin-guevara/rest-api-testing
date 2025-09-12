a8b8487b76b59314e1f91fab04894e8a465d5a0d
edwin-guevara <24eguevara@gmail.com>
2025-09-11 20:21:27 -0400

    Update index.js

--- repository/index.js
+++ repository/index.js
@@ -4,20 +4,13 @@ const joi = require('@hapi/joi');
 const path = require('path');
 const fs = require('fs');
 const routes = [];
-const routesPath = path.join(__dirname, 'routes');
+const routesPath = path.join(__dirname);
 const fileNames = fs.readdirSync('./routes');
 //fileNames.forEach(
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: 'localhost'
-  });
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Hello World!';
-    }
   });
   await server.start();
   console.log('Server running on %s', server.info.uri);
