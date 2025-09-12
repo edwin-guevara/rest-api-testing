14c3bac52563a43ba32f098b03b60c7ba345d1ec
edwin-guevara <24eguevara@gmail.com>
2025-09-11 21:09:01 -0400

    Update courses.js

--- repository/routes/courses.js
+++ repository/routes/courses.js
@@ -1,2 +1,29 @@
 const Joi = require('@hapi/joi');
 module.exports = [];
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+    server.route({
+        method: 'GET',
+        path: '/courses/subject',
+        handler: (request, h) => {
+            return 'subject';
+        }
+    });
+    server.route({
+      method: 'GET',
+      path: '/courses/credits/number',
+      handler: (request, h) => {
+        return 'number';
+      }
+    });
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+init();
