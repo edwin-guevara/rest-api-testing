5a6938d5f4a405550d973bcd4f5ada3bd55e645a
edwin-guevara <24eguevara@gmail.com>
2025-09-18 23:05:24 -0400

    Update courses.js

--- repository/controllers/courses.js
+++ repository/controllers/courses.js
@@ -1,14 +1,7 @@
-const courseServices = require('../services/courses');
 class CourseController {
   getCoursesBySubject(request) {
-    const subjectCode = request.subjectCode;
-    const result = await courseServices.getCoursesBySubject(subjectCode);
-    return JSON.stringify(result);
   }
   getGenEdCoursesByCategory(request) {
-    const category = request.category;
-    const result = await courseServices.getCoursesByGenEdCategory(category);
-    return JSON.stringify(result);
   }
 }
 module.exports = new CourseController();
