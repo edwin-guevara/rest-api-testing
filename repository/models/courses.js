675b1f24570a10374b221abba5f32a2efb520c8c
edwin-guevara <24eguevara@gmail.com>
2025-09-18 21:42:41 -0400

    Update courses.js

--- repository/models/courses.js
+++ repository/models/courses.js
@@ -1,6 +1,9 @@
-const promises = require('fs.promises');
+const fsPromises = require('fs.promises');
 const path = require('path');
 const coursesPath = path.join(__dirname);
 class Courses {
-  async() {
+  constructor() {
+    
+  }
+}
     
