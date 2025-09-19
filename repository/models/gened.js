efa274d53a210f5150b99f719f85694de531e44d
edwin-guevara <24eguevara@gmail.com>
2025-09-18 22:04:11 -0400

    Update gened.js

--- repository/models/gened.js
+++ repository/models/gened.js
@@ -1,1 +1,11 @@
-
+/*const fs = require('fs').promises;
+const path = require('path');
+class GenEd {
+  async find(criteria = () => true) {
+    const genedPath = path.join(__dirname, 'gened.json');
+    const data = await fs.readFile(genedPath, 'utf-8');
+    const geneds = JSON.parse(data);
+    return geneds.filter(criteria);
+  }
+}
+module.exports = new GenEd();*/
