3c20a4e06e61749e1693558694410400c7d1403d
edwin-guevara <24eguevara@gmail.com>
2025-09-18 22:02:06 -0400

    Update courses.js

--- repository/models/courses.js
+++ repository/models/courses.js
@@ -1,9 +1,13 @@
-const fsPromises = require('fs.promises');
+const fsPromises = require('fs').promises;
 const path = require('path');
-const coursesPath = path.join(__dirname);
 class Courses {
-  constructor() {
-    
-  }
+  //async find(criteria = () => true) {
+    //const coursesPath = path.join(__dirname, 'courses.json');
+    //const data = await fs.readFile(coursesPath, 'utf-8');
+    //const courses = JSON.parse(data);
+    //return courses.filter(criteria);
+  //}
 }
+module.exports = new Courses();
+
     
