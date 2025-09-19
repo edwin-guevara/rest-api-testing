c7799fd59f83c680387a673f28c91636cf9089d9
edwin-guevara <24eguevara@gmail.com>
2025-09-18 22:13:28 -0400

    Update courses.js

--- repository/services/courses.js
+++ repository/services/courses.js
@@ -1,9 +1,13 @@
+const coursesModel = require('../models/courses');
+const genedModel = require('../models/gened');
 class CourseServices {
-  getCoursesBySubject(subjectCode) {
-    return subjectCode;
+  async getCoursesBySubject(subjectCode) {
+    return await coursesModel.find(course => course.courseCode.startsWith(subjectCode));
   }
-  getCoursesByGenEdCategory(category) {
-    return category;
+  async getCoursesByGenEdCategory(category) {
+    const allCourses = await coursesModel.find();
+    const offeredCodes = new Set(allCourses.map(c => c.courseCode));
+    return await genedModel.find(gened => gened.category === category && offeredCodes.has(gened.courseCode));
   }
 }
 module.exports = new CourseServices();
